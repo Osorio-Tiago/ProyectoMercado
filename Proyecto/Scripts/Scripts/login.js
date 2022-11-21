73f7b451ddc19d320d70a2f22838ceef170baa8b
marivar08 <mariana0810@hotmail.es>
2022-11-20 23:53:51 -0600

    cambios finales live share

--- Proyecto/Scripts/Scripts/login.js
+++ Proyecto/Scripts/Scripts/login.js
@@ -7,5 +7,33 @@ var url = "/Cajero/"
 
 //funcion principal
 function iniciarSesion() {
-    
+   
+    fetch(url + "IniciarSesion", {
+        method: "POST",
+        body: JSON.stringify({
+            UsuarioLogin: document.getElementById("username").value , 
+            ContraLogin: document.getElementById("password").value  
+          
+        }),
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        }
+    }).then(function (response) {
+        if (response.ok)
+            return response.text()
+        else
+            document.location.href = "/Error.cshtml"
+    }).then(function (Data) {
+        EstadosDeRespuesta = JSON.parse(Data);
+        if (EstadosDeRespuesta.StatusDescription == "OK") {
+            
+        }
+        else {
+            document.getElementById("usuarioIncorrecto").innerHTML = `<div>Error al iniciar sesion</div>`
+
+        }
+
+    })
+
 }
