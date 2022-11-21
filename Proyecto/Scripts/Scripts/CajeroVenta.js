9712dd294adb0b0c35ce2c7cbb5a6a180f76edf7
marivar08 <mariana0810@hotmail.es>
2022-11-20 21:23:39 -0600

    java scripts y html de gerente general y gerente de area

---
+++ Proyecto/Scripts/Scripts/CajeroVenta.js
@@ -0,0 +1,19 @@
+﻿var idProducto = document.getElementById("idProducto");
+var tipoProducto = document.getElementById("tipoProducto");
+var modalHead = document.getElementById("consultarInformacion");
+var modalBody = document.getElementById("mostrarInformacion") 
+
+//Objeto para leer posibles respuestas del servidor
+var url = "/Cajero/"
+
+//funcion principal
+function consultarProducto() {
+    fetch(url + "Cajero_Consultar") //Nombre pendiente a ver cual le pone Santiago
+    .then((respuesta) => {
+        return respuesta.json();
+    }).then((datos) => {
+        var producto = '';
+        //En la variable producto se pone la información en lenguaje html que se mostrará en el modal de Bootstrap
+        document.getElementById("listaMarcas").innerHTML = producto
+    });
+}
